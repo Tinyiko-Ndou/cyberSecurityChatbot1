b07578e4458cd454120e9b37112e75984214bcfc
Tinyiko-Ndou <ST10490357@rcconnect.edu.za>
2026-04-12 15:51:35 +0200

    Welcome message and name prompt

--- cyberSecurityChatbot1/Program.cs
+++ cyberSecurityChatbot1/Program.cs
@@ -1,10 +1,34 @@
 ﻿namespace cyberSecurityChatbot1
 {
+    using System;
+    using System.Media;
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            PlayGreeting.Play();
+            Console.WriteLine("Welcome to the Cyber Security Awareness Bot!");
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+
+            Console.WriteLine("Welcome, " + name + "!!");
+
+            while (true)
+            {
+                Console.Write("\nHow can i assit you (exit to Quit): ");
+                string input = Console.ReadLine().ToLower();
+
+                if (input == "exit")
+                {
+                    Console.WriteLine("Goodbye!!");
+                    break;
+                }
+               // Bot.Respond(input, name);
+            }
+
+
+
         }
     }
 }
---
+++ cyberSecurityChatbot1/greeting.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Media;
+
+namespace cyberSecurityChatbot1
+{
+    internal class PlayGreeting
+    {
+        public static void Play()
+        {
+            try
+            {
+                SoundPlayer player = new SoundPlayer("welcome.wav");
+                
+                player.PlaySync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error playing sound: " + ex.Message);
+            }
+        }
+    }
+}
