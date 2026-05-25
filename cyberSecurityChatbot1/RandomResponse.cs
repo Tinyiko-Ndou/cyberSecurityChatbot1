59a1e0bf7f677ec19632a946e945caa4ba6c4a88
Tinyiko-Ndou <ST10490357@rcconnect.edu.za>
2026-05-25 16:12:55 +0200

    random response class created

---
+++ cyberSecurityChatbot1/RandomResponse.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace cyberSecurityChatbot1
+{
+    internal class RandomResponses
+    {
+        static Random random = new Random();
+        public static string GetPasswordTip()
+        {
+            string[] responses =
+            {
+
+                 "Use strong passwords with symbols and numbers.",
+                 "Avoid using your birthday as a password.",
+                 "A password manager can help keep passwords secure.",
+                  "Use different passwords for different accounts."
+            };
+            return responses[random.Next(responses.Length)];
+        }
+        public static string GetPhishingTip()
+        {
+            string[] responses =
+            {
+                "Never click suspicious email links.",
+                  "Always verify the sender’s email address.",
+                     "Phishing emails often create urgency to scare users.",
+                       "Avoid downloading attachments from unknown senders."
+
+            };
+            return responses[random.Next(responses.Length)];
+        }
+        public static string GetSafeBrowsingTip()
+        {
+            string[] responses =
+            {
+             "Always check for HTTPS in website URLs.",
+              "Avoid public Wi-Fi for sensitive activities.",
+               "Keep your browser updated.",
+               "Do not download files from unknown websites."
+            };
+            return responses[random.Next(responses.Length)];
+        }
+    }
+}
+
